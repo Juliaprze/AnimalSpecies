5af6731e6cd41df7ed2dd26b69f5e4dcbce8ceba
Julia Przebinda <julia.przebinda@microsoft.wsei.edu.pl>
2025-02-13 12:22:03 +0100

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Species hawk = new Species("Hawk", "Bird", 80, "A bird of prey.");
+    Species lion = new Species("Lion", "Mammal", 38, "King of the jungle.");
+    Species shark = new Species("Shark", "Fish", 82, "A large predatory fish.");
+
+    hawk.printData();
+    lion.printData();
+    shark.printData();
+
+        System.out.println("\nCloned species:");
+        Species clonedShark = shark.cloneObject();
+        clonedShark.printData();
     }
 }
--- src/Species.java
+++ src/Species.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Species {
+public class Species {
+    String animalName, speciesName, description;
+    int chromosomeNum;
+
+    Species(String animalName, String speciesName, int chromosomeNum, String description){
+        this.animalName = animalName;
+        this.speciesName = speciesName;
+        this.chromosomeNum = chromosomeNum;
+        this.description = description;
+    }
+
+    String getFullName(){
+        return speciesName + " " + animalName;
+    }
+
+    int getHaploidNumber(){
+        return chromosomeNum / 2;
+    }
+
+    void printData(){
+        System.out.println(getFullName() + ", Chromosomes:" + chromosomeNum + ", haploid number:" + getHaploidNumber());
+        System.out.println("Description: " + description);
+        System.out.println();
+    }
+
+    Species cloneObject(){
+        return new Species(animalName, speciesName, chromosomeNum, description);
+    }
 }
+
